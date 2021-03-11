50af72a92dc48b72d3933e7e4289a937d7a12af7
elahe412 <elahe.es412@yahoo.com>
2021-03-11 17:33:59 +0330

    initial commit 991221

---
+++ apps/post/templatetags/post_custom_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+
+
+@register.filter(name='age')
+def post_age(publish_date):
+    age = timezone.now() - publish_date
+    days, seconds = age.days, age.seconds
+    hours = days * 24 + seconds // 3600
+    month = days // 30
+    if hours <= 1:
+        return ' few moments ago'
+    if days <= 1:
+        return '{} hours ago'.format(hours)
+    if days <= 30:
+        return '{} days ago'.format(days)
+    if days <= 365:
+        return '{} months ago'.format(month)
